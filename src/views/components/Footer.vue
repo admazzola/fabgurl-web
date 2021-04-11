33cefc41b37b7e875c266b7ed8c63d200c851f4e
andy <admazzola@gmail.com>
2021-04-11 11:26:45 -0400

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -5,13 +5,13 @@
         <div class="column-5 w-col w-col-4 w-col-small-4">
           <h3 class="heading-3 footer">Documentation</h3>
 
-
+           <a href="/"   class="footer-link">Home</a>
           <a href="https://docs.burnbook.io" target="_blank" class="footer-link">Burnbook Docs</a>
           <a href="https://etherscan.io/address/0x5a0361e743b1d172dc09456388aa3d4acd14adf3" target="_blank" class="footer-link">Smart Contract</a>
           </div>
         <div class="column-6 w-col w-col-4 w-col-small-4">
           <h3 class="heading-3 footer">Community &amp; Social</h3>
-            
+              <a href="https://digitaltoast.0xbtc.io" target="_blank" class="footer-link">Digital Toast</a>
 
         <div class="column-7 w-col w-col-4 w-col-small-4 flex flex-col h-full">
 
