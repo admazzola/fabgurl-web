9f712906f1e1454a839eece1811fa73cd6aa9ab0
andy <andy@jonezie.com>
2021-04-12 01:04:18 -0400

    toast v2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -201,7 +201,7 @@ export default {
            async fetchOwnedToast(){
 
             let apiURI = 'https://api.starflask.com/api/v1/testapikey'
-            let inputData = {requestType: 'ERC721_balance_by_token', input: { token:'0xb56b78fa93b439953973be5cbeb40d2dc15cea8d' } } 
+            let inputData = {requestType: 'ERC721_balance_by_token', input: { token:'0x9b2214d749f736873b2232a54889342610878651' } } 
             let results = await StarflaskAPIHelper.resolveStarflaskQuery(apiURI ,  inputData  )
             
 
